51998e54491b2355df53db6d8ab029719f0c57b3
tombuildsstuff <git@ibuildstuff.co.uk>
2019-10-11 11:20:04 +0200

    r/linux_virtual_machine_scale_set: auto-scaling example

--- examples/vm-scale-set/linux/basic/main.tf
+++ examples/vm-scale-set/linux/basic/main.tf
@@ -18,13 +18,14 @@ resource "azurerm_subnet" "internal" {
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "main" {
-  name                = "${var.prefix}-vmss"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  sku                 = "Standard_F2"
-  instances           = 3
-  admin_username      = "adminuser"
-  admin_password      = "P@ssw0rd1234!"
+  name                            = "${var.prefix}-vmss"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  sku                             = "Standard_F2"
+  instances                       = 3
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"
+  disable_password_authentication = false
 
   source_image_reference {
     publisher = "Canonical"
