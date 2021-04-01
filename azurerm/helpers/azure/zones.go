4aca25d24933919176af7106b24494e6cb560c09
ArcturusZhang <dapzhang@microsoft.com>
2021-04-01 17:53:37 +0800

    add more helpers

--- azurerm/helpers/azure/zones.go
+++ azurerm/helpers/azure/zones.go
@@ -3,6 +3,7 @@ package azure
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
 func SchemaZones() *schema.Schema {
@@ -75,6 +76,32 @@ func FlattenZones(v *[]string) []interface{} {
 
 	for _, s := range *v {
 		zones = append(zones, s)
+	}
+	return zones
+}
+
+func Track2ExpandZones(v []interface{}) *[]*string {
+	zones := make([]*string, 0)
+	for _, zone := range v {
+		zones = append(zones, utils.String(zone.(string)))
+	}
+	if len(zones) > 0 {
+		return &zones
+	} else {
+		return nil
+	}
+}
+
+func Track2FlattenZones(v *[]*string) []interface{} {
+	zones := make([]interface{}, 0)
+	if v == nil {
+		return zones
+	}
+
+	for _, s := range *v {
+		if s != nil {
+			zones = append(zones, *s)
+		}
 	}
 	return zones
 }
