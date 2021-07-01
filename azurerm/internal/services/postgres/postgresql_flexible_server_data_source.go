0448efbe64142e12ec36d36717f1747ff81fe4ea
yupwei68 <yupwei@microsoft.com>
2021-07-01 11:24:39 +0800

    update

--- azurerm/internal/services/postgres/postgresql_flexible_server_data_source.go
+++ azurerm/internal/services/postgres/postgresql_flexible_server_data_source.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/preview/postgresql/mgmt/2020-02-14-preview/postgresqlflexibleservers"
+	"github.com/Azure/azure-sdk-for-go/services/postgresql/mgmt/2021-06-01/postgresqlflexibleservers"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/location"
@@ -108,19 +108,22 @@ func dataSourceArmPostgresqlFlexibleServerRead(d *pluginsdk.ResourceData, meta i
 	d.Set("location", location.NormalizeNilable(resp.Location))
 	if props := resp.ServerProperties; props != nil {
 		d.Set("administrator_login", props.AdministratorLogin)
-		d.Set("storage_mb", props.StorageProfile.StorageMB)
+		d.Set("storage_mb", props.Storage.StorageSizeGB)
 		d.Set("version", props.Version)
 		d.Set("cmk_enabled", props.ByokEnforcement)
 		d.Set("fqdn", props.FullyQualifiedDomainName)
-		d.Set("public_network_access_enabled", props.PublicNetworkAccess == postgresqlflexibleservers.ServerPublicNetworkAccessStateEnabled)
 
-		if props.DelegatedSubnetArguments != nil {
-			d.Set("delegated_subnet_id", props.DelegatedSubnetArguments.SubnetArmResourceID)
+		if props.Network != nil {
+			d.Set("public_network_access_enabled", props.Network.PublicNetworkAccess == postgresqlflexibleservers.ServerPublicNetworkAccessStateEnabled)
+			d.Set("delegated_subnet_id", props.Network.DelegatedSubnetResourceID)
 		}
 
-		if storage := props.StorageProfile; storage != nil {
-			d.Set("storage_mb", storage.StorageMB)
-			d.Set("backup_retention_days", storage.BackupRetentionDays)
+		if storage := props.Storage; storage != nil {
+			d.Set("storage_mb", storage.StorageSizeGB)
+		}
+
+		if backup := props.Backup; backup != nil {
+			d.Set("backup_retention_days", backup.BackupRetentionDays)
 		}
 	}
 
